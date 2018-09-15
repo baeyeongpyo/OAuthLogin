c5c2fe2ed0a515b0c128e39fd8ffa24ae8f0d4d2
Baecode <byp93@naver.com>
2018-09-15 15:55:05 +0900

    FireBase with Google Login 기능 추가

--- socialoauth/src/main/java/com/example/baeyongpyo/oauthlogin/LoginModule/Fragment/LoginFragment.kt
+++ socialoauth/src/main/java/com/example/baeyongpyo/oauthlogin/LoginModule/Fragment/LoginFragment.kt
@@ -3,16 +3,19 @@ package com.example.baeyongpyo.oauthlogin
 import android.content.Intent
 import android.os.Bundle
 import android.support.v4.app.Fragment
-import android.support.v4.app.FragmentActivity
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.baeyongpyo.oauthlogin.LoginModule.LoginAdapter.KakaoSDKAdpater
+import com.example.baeyongpyo.oauthlogin.LoginModule.LoginSession.GoogleSession.GoogleLogin
 import com.example.baeyongpyo.oauthlogin.LoginModule.Platform
 import com.google.android.gms.auth.api.Auth
 import com.google.android.gms.auth.api.credentials.CredentialPickerConfig.Prompt.SIGN_IN
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.kakao.auth.ISessionCallback
 import com.kakao.auth.KakaoSDK
 import com.kakao.auth.Session
@@ -22,9 +25,10 @@ import kotlinx.android.synthetic.main.fragment_login.*
 class LoginFragment : Fragment() {
 
     private val fragCtx by lazy { activity?.applicationContext!! }
-
-    var loginSuccess : ()->Unit ={}
-    var loginFail : (String) -> Unit = {}
+    private val mAuth by lazy { FirebaseAuth.getInstance() }
+    private val googleLogin by lazy { GoogleLogin(activity!!).client }
+    var loginSuccess: () -> Unit = {}
+    var loginFail: (String) -> Unit = {}
 
     private val kakaoLoginCallback by lazy { Platform.KAKAO.login(loginSuccess, loginFail) as ISessionCallback }
     private val KakaoSession by lazy {
@@ -41,19 +45,11 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         KakaoSDK.init(KakaoSDKAdpater(fragCtx))
-        KakaoSession
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("web client id")
-                .requestEmail()
-                .build()
-
-        val mGoogleApiClient = GoogleApiClient.Builder(fragCtx)
-//                .enableAutoManage(this@LoginFragment as FragmentActivity) { }
-                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-                .build()
-
+        kakao_login_button.setOnClickListener {
+            KakaoSession
+        }
         google_login_button.setOnClickListener {
-            Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient).let {
+            Auth.GoogleSignInApi.getSignInIntent(googleLogin).let {
                 startActivityForResult(it, SIGN_IN)
             }
         }
@@ -61,7 +57,26 @@ class LoginFragment : Fragment() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if (KakaoSession.handleActivityResult(requestCode, resultCode, data)) return
+        if (requestCode == SIGN_IN) {
+            GoogleSignIn.getSignedInAccountFromIntent(data).run {
+                getResult(ApiException::class.java).run { fireBaseAuth(idToken.toString()) }
+            }
+        }
         super.onActivityResult(requestCode, resultCode, data)
+
+    }
+
+    private fun fireBaseAuth(tokenToString: String) {
+        GoogleAuthProvider.getCredential(tokenToString, null).let { mAuth.signInWithCredential(it) }
+                .addOnCompleteListener(activity!!) {
+                    if (it.isSuccessful) {
+                        mAuth.currentUser.let { fragCtx.toast(it.toString()) }
+                    } else {
+                        fragCtx.toast("Fail FireBase")
+                        Log.e("fireBaseErrorMessage", it.exception.toString())
+                    }
+                }
+
     }
 
     override fun onDestroy() {
